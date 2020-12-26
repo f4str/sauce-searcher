85e5a4e9d7caf24693aee95efd1331695228b4f3
Farhan Ahmed <farhan.ahmed.1@stonybrook.edu>
2020-12-26 00:16:21 -0500

    finished light novel info display

--- src/components/Anime.jsx
+++ src/components/Anime.jsx
@@ -28,7 +28,7 @@ const Anime = forwardRef(({query, active}, ref) => {
 	const [endings, setEndings] = useState([]);
 	
 	const fetchData = async () => {
-		setMessage(<Loader key='loader' active inline='centered' size='large'>Loading</Loader>);
+		setMessage(<Loader key='loader' active inline='centered' size='large'>Searching</Loader>);
 		setFound(false);
 		
 		const first = query.charAt(0);
@@ -73,12 +73,12 @@ const Anime = forwardRef(({query, active}, ref) => {
 	
 	const textGridRow = (name, data) => {
 		if (data && data !== '') {
-			return <Grid.Row key={name} style={{marginBottom: '5px'}}><span className='bold'>{name}</span>{data}</Grid.Row>
+			return <Grid.Row key={name} style={{marginBottom: '10px'}}><span className='bold'>{name}</span>{data}</Grid.Row>
 		}
 		else {
 			return null;
 		}
-	}
+	};
 	
 	return (
 		<div>
@@ -98,7 +98,7 @@ const Anime = forwardRef(({query, active}, ref) => {
 							<a href={url} className='link'><Header inverted textAlign='left'>{title}</Header></a>
 						</Grid.Row>
 						{textGridRow('', titleEnglish)}
-						<Grid.Row style={{marginBottom: '5px'}}>
+						<Grid.Row style={{marginBottom: '10px'}}>
 							<span className='bold'>Type: </span>{type} | <span className='bold'>Episodes: </span>{episodes}
 						</Grid.Row>
 						{textGridRow('Status: ', status)}
