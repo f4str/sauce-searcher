189b00f201b6c4842664aebaac602078c850aca4
Farhan Ahmed <farhan.ahmed.1@stonybrook.edu>
2021-07-05 16:39:02 -0400

    edge case

--- src/components/Anime.tsx
+++ src/components/Anime.tsx
@@ -90,10 +90,10 @@ const Anime = ({ query }: AnimeProps): React.ReactElement => {
     return (
       <Container className='query'>
         <Grid columns={2} textAlign='left'>
-          <Grid.Column computer={4} largeScreen={4} tablet={6} mobile={6}>
+          <Grid.Column computer={4} widescreen={4} largeScreen={4} tablet={6} mobile={6}>
             <BlurredImage imageUrl={imageUrl} score={score} imageNSFW={false} />
           </Grid.Column>
-          <Grid.Column computer={12} largeScreen={12} tablet={10} mobile={10}>
+          <Grid.Column computer={12} widescreen={12} largeScreen={12} tablet={10} mobile={10}>
             <HeaderGridRow title={title} url={url} />
             <TextGridRow label='English Title' text={titleEnglish} />
             <TextGridRow label={['Type', 'Episodes']} text={[type, episodes]} />
