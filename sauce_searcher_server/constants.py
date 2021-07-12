ad2cf437b9b9476b0cf3382a2f6cf53361852091
Farhan Ahmed <farhaahmed@cs.stonybrook.edu>
2021-07-12 01:37:39 -0400

    finished doujin parser

--- sauce_searcher_server/constants.py
+++ sauce_searcher_server/constants.py
@@ -6,3 +6,6 @@ ANIME_ID_QUERY = 'https://api.jikan.moe/v3/anime/'
 MANGA_ID_QUERY = 'https://api.jikan.moe/v3/manga/'
 LIGHT_NOVEL_ID_QUERY = 'https://api.jikan.moe/v3/manga/'
 DOUJIN_ID_QUERY = 'https://nhentai.net/api/gallery/'
+
+DOUJIN_BASE_URL = 'https://nhentai.net/g/'
+DOUJIN_BASE_IMAGE = 'https://t.nhentai.net/galleries/'
